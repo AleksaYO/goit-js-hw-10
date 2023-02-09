d7d3015ed789f9255970342a935ae77c81eca2a1
AleksaYO <yura.aleksa.98@gmail.com>
2023-02-09 19:35:00 +0200

    Task done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,10 @@
+export default function fetchCountries(name) {
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,56 +11,67 @@ const box = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onSerchCountry, DEBOUNCE_DELAY));
 
-function fetchCountries(name) {
-  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => onCreateEl(data));
-}
-
 function onSerchCountry(e) {
   e.preventDefault();
-  const name = e.target.value;
+  const name = e.target.value.trim();
   if (name === '') {
+    list.innerHTML = '';
+    box.innerHTML = '';
     return;
   }
 
-  fetchCountries(name.trim());
+  fetchCountries(name)
+    .then(onCreateEl)
+    .catch(error => Notiflix.Notify.failure(`${error}`));
 }
 
 function onCreateEl(data) {
   if (data.length > 10) {
-    Notiflix.Notify.info(
-      'Too many matches found. Please enter a more specific name.'
-    );
-    return;
-  } else if ((data.length >= 2) & (data.length <= 10)) {
-    const murcup = data
-      .map(
-        ({ name, flags }) =>
-          `<li class="county-list__item">
-              <img src="${flags.svg}" alt="#" width="25" />
-              <p class="county-list__text">${name}</p>
-            </li>`
-      )
-      .join('');
-    list.innerHTML = murcup;
-    return;
+    onSearchError();
+  } else if (data.length >= 2 && data.length <= 10) {
+    onCreateListEl(data);
   } else if (data.length === 1) {
-    const marcup = data
-      .map(
-        ({ name, flags, population, leanguages, capital }) => `      
+    onCreateBoxEl(data);
+  }
+}
+function onSearchError() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+  return;
+}
+
+function onCreateListEl(data) {
+  const murcup = data
+    .map(
+      ({ name, flags }) =>
+        `<li class="county-list__item">
+              <img src="${flags.svg}" alt="${flags.alt}" width="25" class="county-list__image" />
+              <p class="county-list__text">${name.official}</p>
+            </li>`
+    )
+    .join('');
+  box.innerHTML = '';
+  list.innerHTML = murcup;
+}
+
+function onCreateBoxEl(data) {
+  const marcup = data
+    .map(
+      ({ name, flags, population, languages, capital }) => `
       <div class="country-info__box">
-        <img src="${flags.svg}" alt="#"  width="25"/>
-        <h2 class="country-info__title">${name}</h2>
+        <img src="${flags.svg}" alt="${
+        flags.alt
+      }"  width="80" class="country-info__image"/>
+        <h2 class="country-info__title">${name.official}</h2>
         <p class="country-info__text">Capital: ${capital}</p>
         <p class="country-info__text">Population: ${population}</p>
-        <p class="country-info__text">Leanguages: ${leanguages}</p>
+        <p class="country-info__text">Leanguages: ${Object.values(
+          languages
+        ).join(', ')}</p>
       </div>`
-      )
-      .join('');
-    box.innerHTML = marcup;
-  }
-  console.log(data);
+    )
+    .join('');
+  list.innerHTML = '';
+  box.innerHTML = marcup;
 }
